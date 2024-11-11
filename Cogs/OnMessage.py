4949b02bd010f6831510ed2abf27efe1afc750a8
Rumnius <bystrygrom@gmail.com>
2024-11-11 22:07:56 +0300

    Omega fix names of threads, ultra difficult work

--- Cogs/OnMessage.py
+++ Cogs/OnMessage.py
@@ -26,7 +26,7 @@ class OnMessage(commands.Cog):
                         else: await message.reply(response)
 
             if message.channel.id == bot.SETTINGS["Guilds"]["MAIN_GUILD"]["Channels"]["RpProfile"]:
-                thread = await message.create_thread(name=f"Проверка {message.author.global_name}")
+                thread = await message.create_thread(name=f"Проверка {message.author.name}")
                 anketolog = message.guild.get_role(bot.SETTINGS["Guilds"]["MAIN_GUILD"]["Roles"]["Anketolog"])
                 await thread.send(anketolog.mention)
                 on_cheking = message.guild.get_role(bot.SETTINGS["Guilds"]["MAIN_GUILD"]["Roles"]["OnCheking"])
