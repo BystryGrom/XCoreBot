2fc8df0849962c10d25bba29b73770c0f3bac064
Rumnius <bystrygrom@gmail.com>
2025-02-24 14:27:24 +0300

    Теги во всех чатах

--- HelpClasses/Tags.py
+++ HelpClasses/Tags.py
@@ -4,8 +4,7 @@ from DataBase import DbWork
 
 class Tags:
     async def check_tag(message: discord.Message, bot_object: commands.Bot):
-        general = bot_object.SETTINGS["Guilds"]["MAIN_GUILD"]["Channels"]["General"]
-        if message.author.id == bot_object.user.id or message.channel.id == general:
+        if message.author.id == bot_object.user.id:
             return
 
         if message.content.startswith("."):
